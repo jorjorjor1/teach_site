7b2e90feb8ea0f5ef2ced4931a16ed682b34761d
SherstnevA <sherartem@gmail.com>
2019-10-10 22:49:30 +0300

    second

--- quiz/apps/questions/db_test.py
+++ quiz/apps/questions/db_test.py
@@ -2,24 +2,23 @@ import sqlite3
 import os.path
 
 
-root_dir = os.path.join(os.getcwd(), 'Django/quiz')
-db_path = os.path.join(root_dir, "db.sqlite3")
-with sqlite3.connect(db_path) as db:
-    cursor = db.cursor()
-    cursor.execute("SET FOREIGN_KEY_CHECKS=0")
+# root_dir = os.path.join(os.getcwd(), 'Django/quiz')
+# db_path = os.path.join(root_dir, "db.sqlite3")
+# with sqlite3.connect(db_path) as db:
 
 #     user = 'admin'
 #     cursor = db.cursor()
-#     sql_record = """INSERT INTO questions_score (default, value, question_id, auth_user.id)
-# FROM questions_score 
-# LEFT JOIN questions_question ON questions_score.question_id = questions_question.id
-# LEFT JOIN auth_user on questions_score.user_id = auth_user.id
-# WHERE auth_user.username = '%s'""" %(user,)
+#     sql_record = """select questions_question.id, question_text, questions_score.score from questions_question 
+#         left join questions_score on questions_question.id = questions_score.question_id
+#         left join questions_quiz on questions_question.quiz_id = questions_quiz.id
+#         LEFT JOIN auth_user on questions_score.user_id = auth_user.id
+#         WHERE auth_user.id = '%s' and questions_quiz.id = '%s'""" %('2', '1')
 #     cursor.execute(sql_record)
 #     results = cursor.fetchall()
 #     print(results)
     
-    #db.close()
+#     db.close()
+
 
 
 #     user = 'admin'
@@ -33,3 +32,21 @@ with sqlite3.connect(db_path) as db:
 #     results = cursor.fetchall()
 #     print(results)
 #     #db.close()
+
+root_dir = os.path.join(os.getcwd(), 'Django/quiz')
+db_path = os.path.join(root_dir, "db.sqlite3")
+with sqlite3.connect(db_path) as db:
+
+    user = 'admin'
+    cursor = db.cursor()
+    sql_record = """select questions_question.id, question_text, questions_score.score from questions_question 
+        left join questions_score on questions_question.id = questions_score.question_id
+        left join questions_quiz on questions_question.quiz_id = questions_quiz.id
+        LEFT JOIN auth_user on questions_score.user_id = auth_user.id
+        WHERE questions_score.user_id = '%s' and questions_quiz.id = '%s'""" %('2', '1')
+
+    cursor.execute(sql_record)
+    results = cursor.fetchall()
+    print(results)
+    
+    db.close()
--- quiz/apps/questions/views.py
+++ quiz/apps/questions/views.py
@@ -16,8 +16,13 @@ def main_screen(request):
     return render(request, 'questions/main_page.html', {'quiz_list':quiz_list})
 
 def questions_list(request):
-    questions_list = Question.objects.all()
-    return render(request, 'questions/question_list.html', {'questions_list': questions_list})
+    try:
+        questions_list = Question.objects.all()
+        return render(request, 'questions/question_list.html', {'questions_list': questions_list})
+
+
+    except:
+        raise Http404 ('Question_list не найден')
 
 
 def question(request, question_id, quiz_id):
@@ -33,6 +38,17 @@ def question(request, question_id, quiz_id):
         variant_3 = question_setup.variant_3
         correct_var = question_setup.correct_var
         value = question_setup.value
+        cursor = connection.cursor()
+        user = request.user.get_username()
+        sql_record_for_question = """SELECT questions_question.question_text, questions_score.score, auth_user.username 
+        FROM questions_score 
+        LEFT JOIN questions_question ON questions_score.question_id = questions_question.id
+        LEFT JOIN auth_user on questions_score.user_id = auth_user.id
+        WHERE auth_user.username = '%s' and questions_question.question_text = '%s'""" %(user, question_setup)
+        cursor.execute(sql_record_for_question)
+        total = cursor.fetchall()
+        cursor.close()
+        #print(user, total)
 
 
 
@@ -42,10 +58,8 @@ def question(request, question_id, quiz_id):
 
 
     if request.POST.get('var') == correct_var:
-        messages.add_message(request, messages.SUCCESS, 'Правильно!')
         cursor = connection.cursor()
         user = request.user.get_username()
-        print(user)
         sql_record = """SELECT questions_question.question_text, questions_score.score, auth_user.username 
         FROM questions_score 
         LEFT JOIN questions_question ON questions_score.question_id = questions_question.id
@@ -53,35 +67,35 @@ def question(request, question_id, quiz_id):
         WHERE auth_user.username = '%s' and questions_question.question_text = '%s'""" %(user, question_setup)
         cursor.execute(sql_record)
         results = cursor.fetchall()
-        print(results, user)
         cursor.close()
-        # if user == [] and results == []: работает для незарег
-        if results == []:
+        if user == "" and results == []:
+            messages.add_message(request, messages.SUCCESS, 'Правильно!')
+        elif user != "" and results == []:
+            player = results[0][2]
+            #print(player, value)
+            messages.add_message(request, messages.SUCCESS, ('Правильно! Игрок {} получает {} очков').format(player, value))
             select_user_id = """select auth_user.id
             from auth_user
             WHERE auth_user.username = '%s'""" %(user,)
             cursor = connection.cursor()
             cursor.execute(select_user_id)
             user_id = cursor.fetchall()
-            print(user_id)
-            #default, value, question_id, auth_user.id
-            print(question_setup)
             select_questions_id = """SELECT id from questions_question where question_text = '%s'""" %(question_setup,)
             cursor.execute(select_questions_id)
             quest_id = cursor.fetchall()
             inserting_score = """INSERT INTO questions_score  (score, question_id, user_id) values ('%s', '%s', '%s')""" %(value, quest_id[0][0], user_id[0][0],) 
-            #values ('%s', '%s', '%s') %(value, question_id, user_id)
             cursor.execute(inserting_score)
             inserted_score = cursor.fetchall()
-
-            print(inserted_score)
             cursor.close()
-
+        elif user != "" and results != []:
+            player = results[0][2]
+            messages.add_message(request, messages.SUCCESS, ('Правильно! Игрок {} отгадал вопрос раньше и получил {} очков').format(player, value))
         cursor.close()
+
         return render(request, 'questions/question.html',
                       {'question_setup': question_setup, "variant_1": variant_1, "variant_2": variant_2,
                        "variant_3": variant_3, 'correct_var':correct_var, 'next_question_id':next_question_id,
-                       'prev_question_id':prev_question_id, 'quiz_id':quiz_id, 'questions_quant':questions_quant, 'value':value, 'results':results})
+                       'prev_question_id':prev_question_id, 'quiz_id':quiz_id, 'questions_quant':questions_quant, 'value':value})
 
 
         #return HttpResponse(request.POST.get('var'))
@@ -103,15 +117,62 @@ def question(request, question_id, quiz_id):
 #
 
 def quiz_details(request, quiz_id):
-    #new_lst = filter(lambda x: x.attr0 == attr0 and x.attr1 == attr1, lst)
+
     try:
         a = Quiz.objects.get(id=quiz_id)
-        x = a.question_set.all()
+        cursor = connection.cursor()
+        user = request.user.get_username()
+        #x = a.question_set.all() 
+        select_questions_list = """select questions_question.id, questions_question.question_text from questions_question where questions_question.quiz_id = '%s'""" %(quiz_id)
+        cursor.execute(select_questions_list)
+        quest_list = cursor.fetchall()
+        x = []
+        for quest in quest_list:
+            x.append(quest)
+        #print(x)
+        
+        select_user_id_in_quiz = """select auth_user.id
+                from auth_user
+                WHERE auth_user.username = '%s'""" %(user,)
+        cursor.execute(select_user_id_in_quiz)
+        user_id = cursor.fetchall()
+
+        select_done_score = """select questions_question.id, question_text, questions_score.score from questions_question 
+        left join questions_score on questions_question.id = questions_score.question_id
+        left join questions_quiz on questions_question.quiz_id = questions_quiz.id
+        LEFT JOIN auth_user on questions_score.user_id = auth_user.id
+        WHERE auth_user.id = '%s' and questions_quiz.id = '%s'""" %(user_id[0][0], quiz_id)
+        cursor.execute(select_done_score)
+        raw_result = cursor.fetchall()
+        #print(raw_result)
+        done_score = []
+        new_data = []
+        id_checked =[]
+        for result in raw_result:
+            done_score.append(result)
+        #print(done_score)
+        for para in done_score:
+            if para[2] != "" and para[0] not in id_checked:
+                diction = [para[0], para[1], para[2]]
+                new_data.append(diction)
+                id_checked.append(para[0])
+        for pair in x:
+            if pair[0] not in id_checked:
+                diction2 = [pair[0], pair[1]]
+                new_data.append(diction2)
+                id_checked.append(pair[0])
+
+            
+
+        print(new_data)
+                    
+
+
 
     except:
         raise Http404 ('Вопрос не найден')
 
-    return render(request, 'questions/question_list.html', {'x': x})
+    return render(request, 'questions/question_list.html', {'x': x, 'new_data':new_data})
 
 
 
